136ffbedd7c0f10953b241cd90f0e8d9dca4323f
Andres Franco <andresfelipefranco@icloud.com>
2026-01-14 22:03:07 -0500

    Agregando etiquetas al bucket via CI/CD V1

--- test.tf
+++ test.tf
@@ -1,21 +1,22 @@
 terraform {
   backend "gcs" {
-    bucket  = "empresa-piru-tfstate"  # El nombre exacto de tu bucket nuevo
-    prefix  = "terraform/state"       # Carpeta donde se guardará el archivo
+    bucket  = "empresa-piru-tfstate"  # El bucket del estado (NO TOCAR)
+    prefix  = "terraform/state"
   }
 }
 
 provider "google" {
   region = "us-central1"
+  # project = "compact-env-197522" # (Opcional si ya lo toma del entorno)
 }
 
-# Declaración de variables
+# --- Declaración de variables ---
 variable "tags" {
   description = "Etiquetas por defecto (Centro de Costo General, Ambiente, etc)"
   type        = map(string)
   
   default = {
-    cost-center = "infra-general"  # Valor por defecto si no se especifica otro
+    cost-center = "infra-general"
     environment = "production"
     managed-by  = "terraform"
   }
@@ -24,8 +25,9 @@ variable "tags" {
 # --- 1. Bucket Data Lake (Centro de costo: Analítica) ---
 resource "google_storage_bucket" "datalake_raw" {
   name          = "empresa-piru-datalake-raw"
-  location      = "EU"
-  
+  location      = "US"
+  force_destroy = true # Útil para pruebas, permite borrar bucket con datos
+
   # Fusionamos las etiquetas comunes con la específica de este bucket
   labels = merge(var.tags, {
     cost-center = "data-analytics" 
@@ -36,32 +38,10 @@ resource "google_storage_bucket" "datalake_raw" {
 resource "google_storage_bucket" "assets_publicos" {
   name          = "empresa-piru-assets-publicos"
   location      = "US"
+  force_destroy = true
 
   # Sobreescribimos el cost-center para este recurso
   labels = merge(var.tags, {
     cost-center = "marketing-web"
   })
-}
-
-# --- 3. Bucket Logs (Centro de costo: Infra) ---
-resource "google_storage_bucket" "infra_logs" {
-  name          = "empresa-piru-infra-logs-01"
-  location      = "US"
-
-  # Aquí quizás usamos el default o forzamos uno de infra
-  labels = merge(var.tags, {
-    cost-center = "infra-ops"
-  })
-}
-
-resource "google_storage_bucket" "default" {
-  name          = "empresa-piru-tfstate" # (Tu nombre de bucket actual)
-  location      = "us-central1"
-  
-  # --- AGREGA ESTO ---
-  labels = {
-    environment = "dev"
-    managed_by  = "github-actions"
-  }
-  # -------------------
 }
